1c1237a6010a9753a68cfc41fd2c97f5ce9992a9
Marieme Dieng <marieme.dieng2@etu.ussein.edu.sn>
2026-05-21 00:29:02 +0000

    Fix: Add PasswordEncoder bean to WebApplication

--- web-middleware/src/main/java/com/pdfservice/web/WebApplication.java
+++ web-middleware/src/main/java/com/pdfservice/web/WebApplication.java
@@ -3,13 +3,23 @@ package com.pdfservice.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EntityScan("com.pdfservice.web.model")
 @EnableJpaRepositories("com.pdfservice.web.repository")
 public class WebApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
+    }
+
+    // AJOUT DU BEAN PASSWORDENCODER MANQUANT
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
