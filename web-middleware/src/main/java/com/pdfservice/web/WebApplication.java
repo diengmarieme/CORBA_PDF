8f476a99a74013e11f909b73cb6fed08fdd8a57c
Marieme Dieng <marieme.dieng2@etu.ussein.edu.sn>
2026-05-21 10:18:37 +0000

    Fix: clean WebApplication and maintain dedicated SecurityConfig

--- web-middleware/src/main/java/com/pdfservice/web/WebApplication.java
+++ web-middleware/src/main/java/com/pdfservice/web/WebApplication.java
@@ -2,33 +2,11 @@ package com.pdfservice.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-@EntityScan("com.pdfservice.web.model")
-@EnableJpaRepositories("com.pdfservice.web.repository")
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 public class WebApplication {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
-    }
-
-    // AJOUT DU BEAN PASSWORDENCODER MANQUANT
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
